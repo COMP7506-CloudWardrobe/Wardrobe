eb69f5f327aeee8298988d83a00c4485cc35a208
wtc <839385081@qq.com>
2023-04-20 23:00:46 +0800

    feat: register user

--- lib/model/User.dart
+++ lib/model/User.dart
@@ -1,13 +1,15 @@
 class User {
-  String? id;
+  int? id;
   String? email;
   String? password;
+  String? userName;
   String? userToken;
 
   User({
     this.id,
     this.email,
     this.password,
+    this.userName,
     this.userToken
   });
 }
