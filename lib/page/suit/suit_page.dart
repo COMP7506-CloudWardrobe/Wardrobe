7c5c07ec6bf437605a694540505ed153ccd3c089
celiaf <feng598164096@qq.com>
2023-04-21 22:23:31 +0800

    suit page

--- lib/page/suit/suit_page.dart
+++ lib/page/suit/suit_page.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:wardrobe/page/suit/suits_list.dart';
 
-class SuitPage extends StatelessWidget {
+
+class SuitPage extends StatefulWidget {
+  @override
+  State<SuitPage> createState() => _SuitPageState();
+
+  int userId;
+
+  SuitPage({super.key, required this.userId});
+}
+
+class _SuitPageState extends State<SuitPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Suit Page'),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Suits'),
+        ),
+        body: SuitPictureList(userId: widget.userId));
   }
 }
